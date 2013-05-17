e97d92f4d9b41d5695ee0f30586a48033718d531
James Rundquist <james.k.rundquist@gmail.com>
2013-05-17 14:46:13 -0400

    Removed redunant http

--- heartbeat.js
+++ heartbeat.js
@@ -1,6 +1,6 @@
 var request = require('request');
-console.log('Requesting [http://'+process.env.DOMAIN+'/imgur]')
-request('http://'+process.env.DOMAIN+'/imgur', function (error, response, body) {
+console.log('Requesting ['+process.env.DOMAIN+'/imgur]')
+request(''+process.env.DOMAIN+'/imgur', function (error, response, body) {
     if (!error && response.statusCode == 200) {
     console.log('  |-> Status: '+ response.statusCode);
   }else{
